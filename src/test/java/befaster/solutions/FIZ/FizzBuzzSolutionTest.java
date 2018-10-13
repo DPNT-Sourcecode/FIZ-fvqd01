073dc755b82f0182831efb0d6f22b8cb894568df
sbx_user1060 <sbx_user1060@ip-10-129-169-180.eu-west-2.compute.internal>
2018-10-13 15:08:43 +0000

    Sat Oct 13 15:08:43 UTC 2018

---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -0,0 +1,16 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzSolutionTest {
+    private FizzBuzzSolution fizzBuzzSolution;
+
+    @Before
+    public void init() {
+        fizzBuzzSolution = new FizzBuzzSolution();
+    }
+
+    @Test
+    public void 
+}
