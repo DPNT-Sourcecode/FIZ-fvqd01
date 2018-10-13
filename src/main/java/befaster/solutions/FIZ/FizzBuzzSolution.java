bbdb2a103ea8a923ff86fc75d50e2cb9b941d82c
sbx_user1060 <sbx_user1060@ip-10-129-169-180.eu-west-2.compute.internal>
2018-10-13 15:44:38 +0000

    Sat Oct 13 15:44:38 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,23 +5,15 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        if ((number % 3 == 0 && number % 5 == 0) ||
-                (number.toString().contains("3")
-                        && number.toString().contains("5")) ||
-                (number % 3 == 0
-                        && number.toString().contains("5")) ||
-                (number % 5 == 0
-                        && number.toString().contains("3")))
-            return "fizz buzz";
 
-        if (number % 3 == 0 || number.toString().contains("3")) {
-            return "fizz";
-        }
-        if (number % 5 == 0 || number.toString().contains("5")) {
-            return "buzz";
-        }
+        String result = "";
+        if (number % 3 == 0 || number.toString().contains("3"))
+            result = "fizz";
 
-        return number.toString();
+        if (number % 5 == 0 || number.toString().contains("5"))
+            result = result == "" ? "buzz" : "fizz buzz";
+
+        return result == "" ? number.toString() : result;
     }
 
 }
