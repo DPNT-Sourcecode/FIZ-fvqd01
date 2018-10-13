7f3de200bab8f44dd30a10026eb9310446bca503
sbx_user1060 <sbx_user1060@ip-10-129-169-180.eu-west-2.compute.internal>
2018-10-13 15:11:43 +0000

    Sat Oct 13 15:11:43 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,5 +1,6 @@
 package befaster.solutions.FIZ;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,5 +13,13 @@ public class FizzBuzzSolutionTest {
     }
 
     @Test
-    public void 
+    public void return_number_when_not_divided() {
+        Assert.assertEquals("1", fizzBuzzSolution.fizzBuzz(1));
+        Assert.assertEquals("2", fizzBuzzSolution.fizzBuzz(2));
+    }
+
+    @Test
+    public void return_fizz_when_divided_3() {
+        Assert.assertEquals("fizz", fizzBuzzSolution.fizzBuzz(3));
+    }
 }
